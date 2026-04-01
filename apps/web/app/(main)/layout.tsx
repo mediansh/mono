b3e7509b53caf930f6f268e8fecfd5ab67decde4
SALT <abdquy@gmail.com>
2026-04-01 18:19:59 +0800

    remove header and footer from home page

--- apps/web/app/(main)/layout.tsx
+++ apps/web/app/(main)/layout.tsx
@@ -1,16 +1,7 @@
-import { Navbar } from "@/components/navbar"
-import { Footer } from "@/components/footer"
-
 export default function MainLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  return (
-    <>
-      <Navbar />
-      {children}
-      <Footer />
-    </>
-  )
+  return <>{children}</>
 }
