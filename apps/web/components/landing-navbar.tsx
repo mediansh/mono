9cf06355bada9a20281209c816909bb0b459e50d
Alby Churven <albychurv@gmail.com>
2026-04-12 20:17:30 +1000

    Revert "fix: navbar logo scrolls instantly to top of page"
    This reverts commit edc4bc9819f469a598d7ce681000485c9a05d289.

--- apps/web/components/landing-navbar.tsx
+++ apps/web/components/landing-navbar.tsx
@@ -128,12 +128,9 @@ export function LandingNavbar() {
           />
 
           {/* Logo */}
-          <button
-            onClick={() => window.scrollTo({ top: 0 })}
-            className="relative z-10 flex cursor-pointer items-center p-2"
-          >
+          <Link href="/" className="relative z-10 flex items-center p-2">
             <Logo symbolOnly className="size-5" />
-          </button>
+          </Link>
 
           {/* Separator */}
           <div className="relative z-10 mx-1 h-5 w-px bg-foreground/10" />
